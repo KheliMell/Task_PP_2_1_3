05e1c9c41061302a3457d2585f301d0dd4bfe7af
KheliMell <khelimellpi@gmail.com>
2022-08-09 19:48:17 +0300

    fix

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -5,23 +5,27 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class AnimalsCage {
+    private Animal animal;
+    private Timer timer;
 
     @Autowired
-    @Qualifier("dog")
-    private Animal animal;
-    @Autowired
-    private Timer Timer;
+    public AnimalsCage(@Qualifier("dog") Animal animal,
+                       @Qualifier("timer") Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
     public Timer getTimer() {
-        return Timer;
+        return timer;
     }
 }
